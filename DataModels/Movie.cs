e77f3ba26244ea8367f3191bc112cbe087af91be
yvngstvnnv <efra_98@hotmail.nl>
2023-10-10 22:15:00 +0200

    Test commit
    Test commit

--- DataModels/Movie.cs
+++ DataModels/Movie.cs
@@ -1,19 +1,27 @@
 public class Movie
 {
-    public string Name;
-    public string Genre;
-    public string Director;
-    public double Rating;
-    public double Price;
-    public string Duration;
+    public string Title { get; set; }
+    public string Genre { get; set; }
+    public string Director { get; set; }
+    public double Rating { get; set; }
+    public double Price { get; set; }
+    public string Duration { get; set; }
+    public string Description { get; set; }
+    public string Showtimes { get; set; }  
+    public string Cast { get; set; }       
+    public bool IsPlaying { get; set; }    
 
-    public Movie(string name, string genre, string director, double rating, double price, string duration)
+    public Movie(string title, string genre, string director, double rating, double price, string duration, string description, string showtimes, string cast, bool isPlaying)
     {
-        Name = name;
+        Title = title;
         Genre = genre;
         Director = director;
         Rating = rating;
         Price = price;
         Duration = duration;
+        Description = description;
+        Showtimes = showtimes;
+        Cast = cast;
+        IsPlaying = isPlaying;
     }
 }
---
+++ Presentation/MovieListing.cs
@@ -0,0 +1,22 @@
+public class MovieListing
+{
+    public static void DisplayMovies(List<Movie> movies)
+    {
+        for (int i = 0; i < movies.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {movies[i].Title} | {movies[i].Duration} | {movies[i].Genre}");
+        }
+    }
+
+    public static void DisplayMovieDetails(Movie movie)
+    {
+        Console.WriteLine($"Title: {movie.Title}");
+        Console.WriteLine($"Duration: {movie.Duration}");
+        Console.WriteLine($"Genre: {movie.Genre}");
+        Console.WriteLine($"Description: {movie.Description}");
+        Console.WriteLine($"Rating: {movie.Rating}");
+        Console.WriteLine($"Showtimes: {movie.Showtimes}");
+        Console.WriteLine($"Cast: {movie.Cast}");
+        Console.WriteLine($"Director: {movie.Director}");
+    }
+}
