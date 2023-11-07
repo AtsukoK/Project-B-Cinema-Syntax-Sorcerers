422687de30bceabb0758137b32a2046fb3117cc9
AtsukoK <lichungtai@hotmail.nl>
2023-11-08 00:10:46 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,47 +15,6 @@ class Program
 {
     public static void Main()
     {
-        List<Chair> Chairs = SmallHallCreation.GenerateSmallHall();
-        int currentROW = 1;
-        int currentChairIndex = 0;
-
-        while (currentChairIndex < Chairs.Count)
-        {
-            if (Chairs[currentChairIndex].Row == currentROW)
-            {
-                Chair chair = Chairs[currentChairIndex];
-
-                if (chair.ID == 0)
-                {
-                    Console.Write("[---]");
-                }
-                else if (chair.ID < 10)
-                {
-                    Console.Write($"[00{chair.ID}]");
-                }
-                else if (chair.ID < 100)
-                {
-                    Console.Write($"[0{chair.ID}]");
-                }
-                else
-                {
-                    Console.Write($"[{chair.ID}]");
-                }
-
-                currentChairIndex++;
-            }
-            else
-            {
-                Console.WriteLine();
-                currentROW++;
-            }
-        }
-        Console.WriteLine("\n");
-        Console.WriteLine("Which filename?");
-        String newfilename = Console.ReadLine();
-
-        string updatedJson = JsonConvert.SerializeObject(Chairs, Formatting.Indented);
-        string jsonFilePath = Path.Combine("Datasources", newfilename); // Datasource/filename
-        File.WriteAllText(jsonFilePath, updatedJson);
+        MainMenu.Start();
     }
 }
